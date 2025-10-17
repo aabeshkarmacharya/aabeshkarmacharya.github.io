4f3b82b57dc7e22f3cd931dd6eb6d07847f5697c
Aabesh Karmacharya <aabesh@onaroll.co>
2025-10-18 03:31:00 +0545

    Update to resume

--- HomeSale/res/custom.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    var hash = window.location.hash;
-    hash && $('ul.nav a[href="' + hash + '"]').tab('show');
-    $('.nav-pills a').click(function () {
-        window.location.hash = this.hash;
-    });
-});
--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# elvisjames.github.io
-
-Project Website
-
-Website "Home Sale" @ https://aabeshkarmacharya.github.io/HomeSale
+# aabeshkarmacharya.github.io
